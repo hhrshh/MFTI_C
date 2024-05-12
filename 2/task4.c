46a869024a420a7ac8ec6c3e52fcad53146fca1e
Mikhailichenko Dmitry <hhrshh@gmail.com>
2024-05-12 18:36:07 +0300

    DZ 2

--- 2/task4.c
+++ 2/task4.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-int main(void) {
+int main(void)
+{
+    printf("\nSolution:\n"
+           "A → B = !A||B\n");
+    printf("----------\n A| B|A→B|\n%2d|%2d|%3d|\n%2d|%2d|%3d|\n%2d|%2d|%3d|\n%2d|%2d|%3d|\n----------\n", 0,0,1, 0,1,1, 1,0,0, 1,1,1);
+    printf("------------\n A| B|!A||B|\n%2d|%2d|%5d|\n%2d|%2d|%5d|\n%2d|%2d|%5d|\n%2d|%2d|%5d|\n------------\n", 0,0,1, 0,1,1, 1,0,0, 1,1,1);
+    printf("identical\n\n");
 
-    printf("Hello world!\n");
+        printf("\nSolution:\n"
+           "A ↔︎ B = (A && B) || (!A && !B)\n");
+    printf("----------\n a| b|A↔︎B|\n%2d|%2d|%3d|\n%2d|%2d|%3d|\n%2d|%2d|%3d|\n%2d|%2d|%3d|\n----------\n", 0,0,1, 0,1,0, 1,0,0, 1,1,1);
+    printf("-----------------------\n a| b|(A&&B)||(!A&&!B)|\n%2d|%2d|%16d|\n%2d|%2d|%16d|\n%2d|%2d|%16d|\n%2d|%2d|%16d|\n-----------------------\n", 
+           0,0,1, 0,1,0, 1,0,0, 1,1,1);
+    printf("identical\n\n");
     return 0;
 }
